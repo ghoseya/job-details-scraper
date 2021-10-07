5e06ac8616a59cc1db45b1be9ff293af484a88be
ghaneshmouthouvel <36722980+ghaneshmouthouvel@users.noreply.github.com>
2021-10-08 04:30:37 +0530

    Update PageExtractor.py

--- PageExtractor.py
+++ PageExtractor.py
@@ -6,9 +6,17 @@ def dataExtraction(url: str):
     parsed_obj = UrlError(url)
     parsed_data = parsed_obj.parser()
     # Extracting TITLE
-    job_title = parsed_data.find('h1', attrs={'class': 'jobsearch-JobInfoHeader-title'}).text.strip()
+    job_title = parsed_data.find(
+        'h1', attrs={'class': 'jobsearch-JobInfoHeader-title'}).text.strip()
     # Extracting INSTITUTION NAME
-    institution_name = parsed_data.find('div', attrs={'class': 'jobsearch-InlineCompanyRating'}).find('div').find('a').text.strip()
+    institution_name = parsed_data.find(
+        'div', attrs={'class': 'jobsearch-InlineCompanyRating'}).find('div').find('a')
+    if institution_name is not None:
+        institution_name = parsed_data.find('div', attrs={
+                                            'class': 'jobsearch-InlineCompanyRating'}).find('div').find('a').text.strip()
+    else:
+        institution_name = parsed_data.find(
+            'div', attrs={'class': 'jobsearch-InlineCompanyRating'}).find('div').text.strip()
     # Extracting LOCATION
     job_location = parsed_data.find(
         'div', attrs={'class': 'jobsearch-JobInfoHeader-subtitle'}).find_all('div')
@@ -16,7 +24,8 @@ def dataExtraction(url: str):
     # Extracting TYPE
     job_type = "Not Mentioned"
     try:
-        job_type_div = parsed_data.find('div', attrs={'id': 'jobDetailsSection'}).find_all('div')
+        job_type_div = parsed_data.find(
+            'div', attrs={'id': 'jobDetailsSection'}).find_all('div')
         for x in range(len(job_type_div)):
             if job_type_div[x].text == "Job Type":
                 job_type = job_type_div[x+1].text.strip()
@@ -25,17 +34,21 @@ def dataExtraction(url: str):
         job_type = "Not Mentioned"
     # Extracting SALARY
     try:
-        job_salary = parsed_data.find('div', attrs={'id': 'jobDetailsSection'}).find('span')
-        if job_salary != None:
+        job_salary = parsed_data.find(
+            'div', attrs={'id': 'jobDetailsSection'}).find('span')
+        if job_salary is not None:
             job_salary = job_salary.text.strip()
         else:
             job_salary = "NaN"
     except:
         job_salary = "NaN"
     # Extracting SUMMARY
-    job_summary = parsed_data.find('div', attrs={'id': 'jobDescriptionText'}).text
+    job_summary = parsed_data.find(
+        'div', attrs={'id': 'jobDescriptionText'}).text
     # Extracting DAYS
-    job_days = parsed_data.find('div', attrs={'class': 'jobsearch-JobMetadataFooter'}).find_all('div')
-    job_days = job_days[1].text.strip()
-
+    job_days_div = parsed_data.find(
+        'div', attrs={'class': 'jobsearch-JobMetadataFooter'}).find_all('div')
+    for x in range(len(job_days_div)):
+        if job_days_div[x].find('a') is not None:
+            job_days = job_days_div[x-1].text.strip()
     return job_title, institution_name, job_location, job_type, job_salary, job_summary, job_days
