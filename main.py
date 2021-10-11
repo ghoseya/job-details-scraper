46c6e9bb2f8f70d8b05fed7e92582b7cb7798266
jagadeesh-tolnut <jagadeeshmouthouvel@gmail.com>
2021-10-12 01:08:50 +0530

    corrected xlsx file handling

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import sys
 from PageExtractor import dataExtraction,field_entities
 import openpyxl
 import os.path
 import time
-import sys
 import pandas as pd
 import pickle
 
@@ -16,27 +16,26 @@ def main():
         read_data_jobs = read_data["url"].tolist()
         workbook = openpyxl.load_workbook(file_name)
         sheet = workbook.active
-        sheet.append(field_entities+["jobs_url"])
-        workbook.save(file_name)
     else:
         read_data_jobs = []
         workbook = openpyxl.Workbook()
         workbook.save(filename=file_name)
         sheet = workbook.active
+        temp_list = field_entities() + ["jobs_url"]
+        sheet.append(temp_list)
         workbook.save(file_name)
     pd.read_excel(file_name)
     for jobs in primary_gen_link:
         print(jobs)
-        if len(primary_gen_link) == len(read_data_jobs):
+        if len(primary_gen_link)==len(read_data_jobs):
             sys.exit()
         if (jobs in read_data_jobs):
             print("Data Present")
-
         else:
             params_data = dataExtraction(jobs)
             sheet.append(params_data + [jobs])
             workbook.save(file_name)
-            time.sleep(2)
+            time.sleep(3)
         workbook.save(file_name)
 
 
