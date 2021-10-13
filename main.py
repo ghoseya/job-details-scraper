575f8752f0ebb1a5364261be657287f4f4c64fce
ghaneshmouthouvel <ghaneshmouthouvel@gmail.com>
2021-10-14 00:21:33 +0530

    Enhanced and Improved File Management

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 from PageExtractor import dataExtraction, field_entities
-import openpyxl
+from openpyxl import Workbook, load_workbook
 from os.path import exists
+from os import makedirs
 from sys import exit
 from time import sleep
 from pandas import read_excel
 from pickle import load
 
 
-def main():
-    file_name = "Data_Librarian.xlsx"
+def main(file_path: str):
     with open("job_urls.txt", "rb") as fp:
         primary_gen_link = list(set(load(fp))) # "set" to have unique elements & "list" to have ordered continuous data 
-    if exists(file_name):
-        read_data = read_excel(file_name)
+    if exists(file_path):
+        read_data = read_excel(file_path)
         read_data_jobs = read_data["jobs_url"].tolist()
-        workbook = openpyxl.load_workbook(file_name)
+        workbook = load_workbook(filename=file_path)
         sheet = workbook.active
-        workbook.save(file_name)
+        workbook.save(filename=file_path)
     else:
         read_data_jobs = []
-        workbook = openpyxl.Workbook()
-        workbook.save(filename=file_name)
+        workbook = Workbook()
+        workbook.save(filename=file_path)
         sheet = workbook.active
         temp_list = field_entities() + ["jobs_url"]
         sheet.append(temp_list)
-        workbook.save(file_name)
+        workbook.save(filename=file_path)
 
 
     for jobs in primary_gen_link:
@@ -35,7 +35,7 @@ def main():
         else:
             params_data = dataExtraction(jobs)
             sheet.append(params_data + [jobs])
-            workbook.save(file_name)
+            workbook.save(filename=file_path)
             print(jobs + " - Completed")
             sleep(3)
 
@@ -43,9 +43,16 @@ def main():
     exit()
 
 if __name__ == "__main__":
+    if exists("datasets"):
+        pass
+    else:
+        makedirs("datasets")
+    file_name = input('Enter filename without extension: ')+'.xlsx'
+    rel_file_path = 'datasets/'+file_name
     while True:
         try:
-            main()
+            main(rel_file_path)
         except AttributeError as e:
+            print(e)
             sleep(7)
-            main()
+            main(rel_file_path)
