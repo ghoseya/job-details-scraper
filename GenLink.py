6149ed117aa2bd51e10fb8d85e7ed5dd9acd74f5
ghaneshmouthouvel <ghaneshmouthouvel@gmail.com>
2021-10-14 15:31:10 +0530

    Fixed Query Generation

--- GenLink.py
+++ GenLink.py
@@ -1,16 +1,18 @@
-import re
+from re import sub
 from UrlParsing import UrlError
+from urllib.parse import urlencode
 import pickle
 
 
 def generateLink():
 
     # generating our own link for a specific job (first page)
-    l1 = "https://www.indeed.com/jobs?q="
-    job = input("job Name:")
+    l1 = "https://www.indeed.com/jobs?"
+    job, location = input("job Name: "), input('Job Location(optional): ')
+    query = {'q':job,'l':location}
     link_list = []
     jobs_list = []
-    start_link = l1 + job
+    start_link = l1 + urlencode(query)
     link_list.append(start_link)
 
     # creating a object to parse the link
@@ -19,9 +21,9 @@ def generateLink():
     count = read.find("div", attrs={"id": "searchCountPages"}).text.strip()
 
     # taking only the number of jobs
-    count = re.sub("Page 1 of ", "", count)
-    count = re.sub(" jobs", "", count)
-    count = re.sub(",", "", count)
+    count = sub("Page 1 of ", "", count)
+    count = sub(" jobs", "", count)
+    count = sub(",", "", count)
     count = int(count)
     page_count = (count // 15) + 1
 
@@ -40,7 +42,7 @@ def generateLink():
 
         for tag in page_url_data.find_all(class_="tapItem"):
             job_id = tag.get('id')
-            job_id = re.sub("job_", "", job_id)
+            job_id = sub("job_", "", job_id)
             specific_job = "https://www.indeed.com/viewjob?jk="
             specific_job = specific_job + job_id
             jobs_list.append(specific_job)
